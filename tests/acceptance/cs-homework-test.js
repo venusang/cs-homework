0fb6a4f91b7e77c874aeac98ca3635dc8daeed1f
Venus Ang <venusang24@gmail.com>
2020-01-04 14:19:12 -0800

    adding acceptance test

---
+++ tests/acceptance/cs-homework-test.js
@@ -0,0 +1,18 @@
+import { module, test } from "qunit";
+import { click, visit, currentURL } from "@ember/test-helpers";
+import { setupApplicationTest } from "ember-qunit";
+
+module("Acceptance | cs homework", function(hooks) {
+  setupApplicationTest(hooks);
+
+  test("visiting /", async function(assert) {
+    await visit("/");
+
+    assert.equal(currentURL(), "/");
+    assert.dom("h2").hasText("Homework Assignment");
+
+    assert.dom("a.button").hasText("Homework Assignment");
+    await click("a.button");
+    assert.equal(currentURL(), "/attachments");
+  });
+});
